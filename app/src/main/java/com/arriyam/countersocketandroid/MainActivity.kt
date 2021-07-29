ad4ecf7a49581ab3c897afc011ffdf11317f30b4
Arriyam <thushen@arriyam.com>
2021-07-28 23:47:12 -0400

    Creating a Button and TextView

--- app/src/main/java/com/arriyam/countersocketandroid/MainActivity.kt
+++ app/src/main/java/com/arriyam/countersocketandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.arriyam.countersocketandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counterBtn = findViewById<Button>(R.id.counterBtn)
+        val countTextView = findViewById<TextView>(R.id.countTextView)
+
+        counterBtn.setOnClickListener{
+
+        }
     }
 }
